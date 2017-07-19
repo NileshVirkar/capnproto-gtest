b15d44819ef7ce2d6316805d9f546f8139ee84c0
Hemant <hemant.bhagat@acellere.com>
2017-07-19 11:17:23 +0530

    Deleted accidently added build folder

--- build/src/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hemant/test-src/capnproto-c++-0.5.3.1/src
-# Build directory: /home/hemant/test-src/capnproto-c++-0.5.3.1/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("kj")
-subdirs("capnp")
